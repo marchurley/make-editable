94d025d4bf6dea89a49389119f8056fa7f5c6314
marchurley <marc@swiss-magic.net>
2017-03-27 10:17:58 +0800

    Finish

---
+++ app.js
@@ -0,0 +1,9 @@
+    var app = angular.module('myApp', []);
+
+    app.directive('makeEditable', function() {
+        return {
+            restrict: 'A',
+            transclude: 'true',
+            templateUrl: 'make-editable.html',
+        };
+    });
